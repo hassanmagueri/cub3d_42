11e24d9e3d7b6050a0eaca37bccc8d2999294946
Elhassane Magueri <emagueri@e3r4p3.1337.ma>
2024-07-17 17:54:50 +0100

    rays in Y top

--- ray.c
+++ ray.c
@@ -6,7 +6,7 @@
 /*   By: emagueri <emagueri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 12:10:59 by emagueri          #+#    #+#             */
-/*   Updated: 2024/07/17 15:32:09 by emagueri         ###   ########.fr       */
+/*   Updated: 2024/07/17 17:54:12 by emagueri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,29 +43,71 @@ t_ray new_ray(t_data *data, double ray_angle)
 	player = data->player;
 	
 	printf("angle : %f\n", radtodeg(ray_angle));
-	y_rect = (((int)player.y / TILE_SIZE) + 1) * TILE_SIZE;
+	// y_rect = (((int)player.y / TILE_SIZE) + 1) * TILE_SIZE;
+	y_rect = (((int)player.y / TILE_SIZE)) * TILE_SIZE;
 	
 	int playerYd = y_rect - player.y;
-	dy = (double)TILE_SIZE * 0 + playerYd;
+	// dy = (double)TILE_SIZE * 0 + playerYd;
+	dy = (double)TILE_SIZE * 0 - playerYd;
 	dx = dy / tan(ray_angle);
-
 	int i = 0;
 
 	while (i < 14)
 	{
-		if (is_wall(data->grid, player.x + dx, player.y + dy))
+		// if (is_wall(data->grid, player.x + dx, player.y + dy))
+		if (is_wall(data->grid, player.x - dx, player.y - dy))
 			break;
-		dy = (double)TILE_SIZE * i + playerYd;
+		// dy = (double)TILE_SIZE * i + playerYd;
+		dy = (double)TILE_SIZE * i - playerYd + 1;
 		dx = dy / tan(ray_angle);
-		i++;		
+		i++;
 	}
-	printf("----------------------------------------\n");
-	len = 100;
 	line = new_line(
 		new_point(player.x, player.y),
-		new_point(player.x + dx, player.y + dy),
+		new_point(player.x - dx, player.y - dy),
+		// new_point(player.x + dx, player.y + dy),
 		SEMI_YELLOW
 	);
 	draw_line(line, player.img);
 	return ray;
 }
+
+// t_ray new_ray(t_data *data, double ray_angle)
+// {
+// 	int			len;
+// 	double		dy;
+// 	double		dx;
+// 	t_ray		ray;
+// 	t_line		line;
+// 	t_player	player;
+// 	int y_rect;
+
+// 	player = data->player;
+	
+// 	printf("angle : %f\n", radtodeg(ray_angle));
+// 	y_rect = (((int)player.y / TILE_SIZE) + 1) * TILE_SIZE;
+	
+// 	int playerYd = y_rect - player.y;
+// 	dy = (double)TILE_SIZE * 0 + playerYd;
+// 	dx = dy / tan(ray_angle);
+
+// 	int i = 0;
+
+// 	while (i < 14)
+// 	{
+// 		if (is_wall(data->grid, player.x + dx, player.y + dy))
+// 			break;
+// 		dy = (double)TILE_SIZE * i + playerYd;
+// 		dx = dy / tan(ray_angle);
+// 		i++;		
+// 	}
+// 	printf("----------------------------------------\n");
+// 	len = 100;
+// 	line = new_line(
+// 		new_point(player.x, player.y),
+// 		new_point(player.x + dx, player.y + dy),
+// 		SEMI_YELLOW
+// 	);
+// 	draw_line(line, player.img);
+// 	return ray;
+// }
